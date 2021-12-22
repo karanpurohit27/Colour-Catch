7e1cbcc961cad4b3ce0b98b1fc490030057bfe62
PratikUp19 <84855152+PratikUp19@users.noreply.github.com>
2021-12-22 15:30:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
+//Declaring the variable 
 let block = document.getElementsByClassName("box");
 let time = document.getElementById("timer");
 let colorsel = document.getElementById("ColSel");
+let disp= document.getElementById("score");
 let sec = 60;
-let start=document.getElementById("start btn-outline-success btn")
 let n=0;
-function Start(){
-    n=1
 
-    let col;
+function Start(){
+    n=1;
+     let col;
      col = colorsel.options[colorsel.selectedIndex].value;
      randomset(col);
-     
-     Begin();
+     cl();
 }
 
 
 
 function countdown(){
-
-    if(sec>0 && sec<=60){
+    if(sec>1 && sec<=60){
         sec--;
         if(sec<10){
             time.innerHTML = "0:0" + sec;
         }
         else{
         time.innerHTML = "0:" + sec;
-
         }
     }
     else{
@@ -33,35 +31,47 @@ function countdown(){
         sec=60;
         time.innerHTML = "1:00";
     }
+    
     return sec;
+    
 }
 
-
 //Setting the timer and running it 
+
+
+
 //Give a random color from the array
 let array = ["blue", "green", "yellow", "red", "voilet", "orange"];
 function randomset(){
     let colors = array[Math.floor(Math.random()*array.length)]; 
+    console.log(colors);
     return colors;
 }
 
 //setting every div a random color
 function set(sel){
-
     let div = block[Math.floor(Math.random()*16)];
     div.style.backgroundColor = sel;
     for(let i = 0 ; i< block.length;i++){
         if(block[i]==div)
-            continue;
+        {
+            
+            continue;}
         block[i].style.backgroundColor = randomset();
     }
+    
+    
 }
 
+function cl()
+{
+    if(n==1)
+    {
+        countdown();
+        set();
+        
+    }
+
+}
+setInterval(cl,1000);
 //changing the colors of divs in every 1 sec
-function Begin(){
-if(n==1){
-    set();
-    countdown();
-}
-}
-setInterval(Begin,1000);
